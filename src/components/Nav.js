7489b899e4517e842fe4a6ba03b50521ac07f171
Bruno <brunochelimo@gmail.com>
2023-04-07 12:49:21 +0300

    added home icon

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { BiHomeAlt, BiUser } from 'react-icons/bi';
+import { BsClipboardData, BsBriefcase, BsChatSquare } from 'react-icons/bs';
+import { Link } from 'react-scroll';
 
 function Nav() {
   return (
-    <div>Nav</div>
-  )
+    <nav>
+      <div className="container mx-auto">
+        <div>
+          <Link>
+            <BiHomeAlt />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;
