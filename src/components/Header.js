199702326ea8f53a96174301712d40de3254ae7d
Bruno <brunochelimo@gmail.com>
2023-04-09 11:49:28 +0300

    done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,35 @@
+// import React from 'react';
+// import logo from '../Assets/logo.png';
+
+// function Header() {
+//   return (
+//     <header className="py-8">
+//       <div className="container mx-auto">
+//         <div className="flex justify-between items-center">
+//           <a href="#">
+//             <img src={logo} alt="" style={{ width: '300px', height: 'auto' }} />
+//           </a>
+//           <button class="btn btn-sm btn-custom font-secondary font-bold blink-animation">Work With Me</button>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
+
+// export default Header;
+
+
 import React from 'react';
 import logo from '../Assets/logo.png';
 
 function Header() {
+  const scrollToWork = () => {
+    const workSection = document.getElementById("work");
+    if (workSection) {
+      workSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <header className="py-8">
       <div className="container mx-auto">
@@ -9,7 +37,9 @@ function Header() {
           <a href="#">
             <img src={logo} alt="" style={{ width: '300px', height: 'auto' }} />
           </a>
-          <button className="btn btn-sm btn-custom">Work With Me</button>
+          <button className="btn btn-sm btn-custom font-secondary font-bold blink-animation" onClick={scrollToWork}>
+            Work With Me
+          </button>
         </div>
       </div>
     </header>
@@ -17,3 +47,4 @@ function Header() {
 }
 
 export default Header;
+
